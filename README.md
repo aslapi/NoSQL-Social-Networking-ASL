61c5e3e13179f3eda4f6f403aabfe72e047cb9f9
Alex LaPierre <aslapierre7@gmail.com>
2024-07-24 20:11:46 -0500

    populated readme, included video

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # NoSQL-Social-Networking-ASL
 Social networking API.
+
+## Description
+
+A NoSQL social media website that has users, thoughts, and reactions to the thoughts!
+
+## Purpose
+
+The purpose of this application is for users to be able to share their thoughts and reactions with others that are apart of the social network application!
+
+## Installation
+
+Go to https://github.com/aslapi/NoSQL-Social-Networking-ASL and clone the repository to your local machine. Once cloned, run "npm i" or "npm install" Once installed, run "npm start" to use the application.
+
+## Usage
+
+Once you've cloned the repository, installed dependencies, and started the application, you can head over to Insomnia to test the routes created.
+
+Below is a link to a video that walks you through how the routes work in Insomnia.
+https://app.screencastify.com/v3/watch/FQafaUbKahZHCEMsWWlD 
--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -67,38 +67,6 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    // async addReaction (req, res) {
-    //     try {
-    //         const thought = await Thought.findOneAndUpdate(
-    //             { _id: req.params.thoughtId },
-    //             { $push: { reactions: req.body } },
-    //             { new: true }
-    //         );
-    //         if (!thought) {
-    //             return res.status(404).json({ message: 'No thought with that ID' });
-    //         }
-    //         res.json(thought);
-    //     }
-    //     catch (err) {
-    //         res.status(500).json(err);
-    //     }
-    // },
-    // async removeReaction(req, res) {
-    //     try {
-    //         const thought = await Thought.findOneAndUpdate(
-    //             { _id: req.params.thoughtId },
-    //             { $pull: { reactions: { reactionId: req.params.reactionId } } },
-    //             { new: true }
-    //         );
-    //         if (!thought) {
-    //             return res.status(404).json({ message: 'No thought with that ID' });
-    //         }
-    //         res.json(thought);
-    //     }
-    //     catch (err) {
-    //         res.status(500).json(err);
-    //     }
-    // }
 
     async addReaction(req, res) {
         try {
